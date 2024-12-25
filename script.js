541a31405d9712763d048d7da98cde1fab9419ae
suryansh04 <suryanshsingh0987@gmail.com>
2024-12-25 15:31:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/** Swiper JS */
+var swiper = new Swiper(".mySwiper", {
+  lazy: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: true,
+  },
+});
