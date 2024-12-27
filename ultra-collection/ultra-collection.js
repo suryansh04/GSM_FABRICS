433e7131372a22dcf75872d9686d7bfdd8c61799
suryansh04 <suryanshsingh0987@gmail.com>
2024-12-27 16:33:51 +0530

    fabric collection pages

---
+++ ultra-collection/ultra-collection.js
@@ -0,0 +1,11 @@
+/** Swiper JS */
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+});
