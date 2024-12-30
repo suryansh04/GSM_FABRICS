b33808e455607497e3f5d9d74906771f02d67e33
suryansh04 <suryanshsingh0987@gmail.com>
2024-12-30 20:40:16 +0530

    changes-(30 dec 2024)

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
-/** Swiper JS */
 var swiper = new Swiper(".mySwiper", {
   lazy: true,
-  slidesPerView: 3,
-  spaceBetween: 20,
   loop: true,
+  spaceBetween: 20,
   autoplay: {
     delay: 2000,
     disableOnInteraction: false,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
   },
 });
 
 /** Hamburger Menu */
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
+const body = document.body;
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   hamburger.classList.toggle("active");
+  body.classList.toggle("no-scroll"); // Toggle no-scroll class
 });
 
 document.querySelectorAll(".nav-links a").forEach((link) => {
   link.addEventListener("click", () => {
     navLinks.classList.remove("active");
     hamburger.classList.remove("active");
+    body.classList.remove("no-scroll"); // Remove no-scroll class when menu is closed
   });
 });
--- ultra-collection/ultra-collection.js
+++ ultra-collection/ultra-collection.js
@@ -13,15 +13,18 @@ var swiper = new Swiper(".mySwiper", {
 /** Hamburger Menu */
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
+const body = document.body;
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   hamburger.classList.toggle("active");
+  body.classList.toggle("no-scroll"); // Toggle no-scroll class
 });
 
 document.querySelectorAll(".nav-links a").forEach((link) => {
   link.addEventListener("click", () => {
     navLinks.classList.remove("active");
     hamburger.classList.remove("active");
+    body.classList.remove("no-scroll"); // Remove no-scroll class when menu is closed
   });
 });
